bb128b1ccca0fca997ec48ef891f425c83711588
Michael Saft <KindReality@Gmail.com>
2021-10-15 12:17:11 -0500

    ~

--- Live/Windows/MainWindow.xaml.cs
+++ Live/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,25 @@ namespace Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort serialPort;
+        Queue<double> samples = new Queue<double>();
         public MainWindow()
         {
             InitializeComponent();
+            serialPort = new SerialPort("COM42");
+            serialPort.DataReceived += SerialPort_DataReceived;
+            serialPort.ErrorReceived += SerialPort_ErrorReceived;
+            serialPort.Open();
+        }
+        private void SerialPort_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            //samples.Enqueue();
         }
     }
 }
